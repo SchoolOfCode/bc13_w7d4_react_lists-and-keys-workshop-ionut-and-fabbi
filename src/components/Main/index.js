c06adcf1cd22cb00d02fd8d2eade4b4f3d3aaddf
fabbiha <sfabbiha@gmail.com>
2022-11-10 16:22:49 +0000

    completed task 2

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -10,8 +10,8 @@ function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
   return <main id="main">
-    {posts.map(({text}) => {
-     return <Post post={text}></Post>
+    {posts.map(({text, postId}) => {
+     return <Post post={text} key={postId}></Post>
     })}
   </main>;
 }
